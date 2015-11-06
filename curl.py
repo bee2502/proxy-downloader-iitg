5c064c55d90ef39e375bb5f16b9b33290d2854d8
Bhagyashree <bee2502@users.noreply.github.com>
2015-11-07 02:18:47 +0530

    Updated code to automatically get size of download

--- curl.py
+++ curl.py
@@ -1,23 +1,23 @@
 import os
+import urllib2
 
-def downloader(url,proxy,size,fname)
+def downloader(url,fname,proxy="",increment=200)
+    data = urllib2.urlopen(url)
+    size= data.headers["Content-Length"]
+    start = 0
+    increment = increment*1024*1024 #default 200 MB increments
+    part = 1
+    curr = start
+    while (start < size):
+    	cmd = "curl -x " + str(proxy) + " -o ubuntu.iso." + str(part) + " -r" + str(start) + "-" + str(curr) + " -L " + str(url) + " & "
+    	os.system(str(cmd))	
+    	start = curr 
+    	curr = curr + inc
+    	part = part + 1
+    cmd = "cat "+str(fname)+".* > "+str(fname)
+
+url = "https://download.fedoraproject.org/pub/fedora/linux/releases/23/"+/
+      "Workstation/x86_64/iso/Fedora-Live-Workstation-x86_64-23-10.iso"
+fname = "fedora.iso"
 proxy = "http://d.harsh:9Harsh@202.141.80.20:3128"
-start = 0
-increment = 200*1024*1024
-end = 2*1024*1024*1024
-fname = #add file name here
-part = 1
-url = "http://cdimage.ubuntu.com/ubuntu-desktop-next/daily-live/current/wily-desktop-amd64.iso"
-
-rce = ri
-
-while (rs < re):
-	tr = "curl -x " + str(proxy) + " -o ubuntu.iso." + str(part) + " -r" + str(rs) + "-" + str(rce) + " -L " + str(url) + " & "
-	os.system(str(tr))	
-	rs = rce
-	rce = rce + ri
-	part = part + 1
-
-#cat fname.* > fname
-
-
+downloader(url,fname,proxy)    
