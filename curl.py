413fdea83a926f8452b7c9d8a5fdba25db18f3e2
harsh-sangwan <harshdeep.sangwan8@gmail.com>
2015-11-06 12:26:09 +0530

    Added Code

---
+++ curl.py
@@ -0,0 +1,23 @@
+import os
+
+def downloader(url,proxy,size,fname)
+proxy = "http://d.harsh:9Harsh@202.141.80.20:3128"
+start = 0
+increment = 200*1024*1024
+end = 2*1024*1024*1024
+fname = #add file name here
+part = 1
+url = "http://cdimage.ubuntu.com/ubuntu-desktop-next/daily-live/current/wily-desktop-amd64.iso"
+
+rce = ri
+
+while (rs < re):
+	tr = "curl -x " + str(proxy) + " -o ubuntu.iso." + str(part) + " -r" + str(rs) + "-" + str(rce) + " -L " + str(url) + " & "
+	os.system(str(tr))	
+	rs = rce
+	rce = rce + ri
+	part = part + 1
+
+#cat fname.* > fname
+
+
